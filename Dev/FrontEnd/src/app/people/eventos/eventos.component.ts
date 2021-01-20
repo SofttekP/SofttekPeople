c56afb72fe35062530573b046ef22bba9ae97959
PeopleSofttek <dcanonq@libertadores.edu.co>
2021-01-20 08:38:11 -0500

    commit calendar

--- Dev/FrontEnd/src/app/people/eventos/eventos.component.ts
+++ Dev/FrontEnd/src/app/people/eventos/eventos.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  TemplateRef,
+} from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
 
 @Component({
-  selector: 'app-eventos',
-  templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.scss']
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html'
 })
-export class EventosComponent implements OnInit {
 
+export class CalendarComponent {
   constructor() { }
+  calendarOptions: CalendarOptions = {
+    themeSystem: 'bootstrap',
+    initialView: 'dayGridMonth',
+    eventClick: this.handleEventClick.bind(this), // bind is important!
+    events: 'https://fullcalendar.io/demo-events.json',
+  };
 
-  ngOnInit(): void {
+  handleDateClick(arg): void {
+    console.log('date click! ' + arg.dateStr);
+  }
+  handleEventClick(arg): void {
+    console.log('event click! ' + arg.dateStr);
   }
 
 }
